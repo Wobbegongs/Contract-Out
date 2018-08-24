ed98bcd6467fd3581101f08b775d4b1552f6cbf8
andwong91 <andwong91@gmail.com>
2018-08-24 12:22:58 -0700

    Temp changes before upstream pull

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { ReactDOM, render } from 'react-dom';
 import styles from './scss/styling.scss';
 
+render(<>, document.getElementById());
