7f7bb977da88730cca1e7b2f2a44387802e77b87
GregDoming <greg.domingue1@gmail.com>
2018-08-24 21:29:43 -0700

    will send text when post request is sent

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
-const accountSid = 'ACfd9e7134b90283d091a1a92ec5ebf1eb';
-const authToken = 'your_auth_token';
-const client = require('twilio')(accountSid, authToken);
+const bodyParser = require('body-parser');
+const messageController = require('./server/messageController');
+
 
 app.use(express.static('dist'))
+app.use(bodyParser.json())
 
-app.post('/sendText', function(req, res) {
-    client.messages.create({
-        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-        from: '+13236132041',
-        to: '+17736362712'
-      })
-     .then(message => console.log(message.sid))
-     .done();
+app.post('/sendText', 
+messageController.sendText,
+ function(req, res) {
+    res.send('hi')
+
 })
 
 app.listen(5000, () => 
---
+++ server/messageController.js
@@ -0,0 +1,28 @@
+const messageController = {};
+const accountSid = 'ACfd9e7134b90283d091a1a92ec5ebf1eb'; //account number to the online phone number
+
+const authToken = '65e624ce3481b398e40b1db5b4719b20'; //24 hour authentication token, if this was production level it would be on our local hardDrive
+const client = require('twilio')(accountSid, authToken);
+
+//middleware that sends a twilio text message
+messageController.sendText = (req, res, next) => {
+    client.messages.create({
+        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+        from: '+13236132041',
+        to: '+17736362712'
+      })
+     .then(message => console.log(message.sid))
+     .done();
+}
+
+messageController.sendEmail = (req, res, next) => {
+    
+}
+
+
+
+
+
+
+
+module.exports = messageController
