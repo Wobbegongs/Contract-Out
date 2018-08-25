63154d09ef3b9a11a2659014c1ab56e4d3d34762
andwong91 <andwong91@gmail.com>
2018-08-24 19:37:25 -0700

    Added labels and line breaks for username and password fields

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -12,8 +12,12 @@ const Signup = withRouter(({ history }) => {
     <div className="signup">
       <h2>Sign up</h2>
       <form>
+        <label htmlFor="uname">Username: </label>
         <input type="text" placeholder="Username" name="uname"/>
+        <br></br>
+        <label htmlFor="pw">Password: </label>
         <input type="password" placeholder="Password" name="pw"/>
+        <br></br>
         <button onClick={() => history.push('/home')}>Register</button>
       </form>
     </div>
