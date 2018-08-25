89245dfd4f13b3c4f376cc87a6526cc631715252
andwong91 <andwong91@gmail.com>
2018-08-24 20:06:11 -0700

    Mock form completed

--- client/components/CreatePost.js
+++ client/components/CreatePost.js
@@ -9,8 +9,36 @@ import {
 
 const CreatePost = withRouter(({ history }) => {
   return (
-    <div>
-    
+    <div id="newPost">
+      <form>
+        <h2>New Post</h2>
+        <label htmlFor="title">Title: </label>
+        <input type="text" placeholder="Title your post" name="title"/>
+        <br></br>
+        <label htmlFor="location">Location: </label>
+        <input type="text" placeholder="Add a location" name="location"/>
+        <br></br>
+        <h3>Tasks: </h3>
+        <input type="number"/>
+        <input type="text"/>
+        <select>
+          <option>Caterer</option>
+          <option>Mover</option>
+          <option>Cleaner</option>
+          <option>Designer</option>
+          <option>Photographer</option>
+          <option>Greeter</option>
+          <option>Demolition</option>
+          <option>Painter</option>
+          <option>Gardener</option>
+          <option>Maintenance</option>
+        </select>
+        <br></br>
+        <label htmlFor="comments">Additional comments (optional): </label>
+        <textarea rows="4" cols="50" name="comments" placeholder="250 words max" maxLength="250"></textarea>
+        <br></br>
+        <button type="submit">Create post</button>
+      </form>
     </div>
   )
 });
